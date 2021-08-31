7cd4c1cfa5efb4523dade69722d7bbf0b34a6589
KwonHyuck <kwonhyuck0218@gmail.com>
2021-08-31 14:53:40 +0900

    first build

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
 
 export default defineUserConfig<DefaultThemeOptions>({
+  dest: 'output',
   lang: 'en-US',
   title: 'GreenZoo Table Of Contents',
   description: 'GreenZoo Table Of Contents',
