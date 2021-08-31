9efadc281971cec996eab9237b350212580d2144
KwonHyuck <kwonhyuck0218@gmail.com>
2021-08-31 15:34:25 +0900

    npm install add && test2

--- source/.vuepress/.temp/internal/siteData.js
+++ source/.vuepress/.temp/internal/siteData.js
@@ -1,7 +1,7 @@
 export const siteData = {
   "base": "/greenzoo-toc/",
   "lang": "en-US",
-  "title": "GreenZoo Table Of Contents",
+  "title": "GreenZoo Table Of Contents Test",
   "description": "GreenZoo Table Of Contents",
   "head": [],
   "locales": {}
