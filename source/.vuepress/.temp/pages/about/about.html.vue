f3bb58b6d547910bc67fba1a3f0d1c1f15199a58
greenzoo-quokka <greenzoo-quokka@users.noreply.github.com>
2021-08-31 11:30:24 +0000

    deploy: 05e847e54db9cf8ca456192ebc9ea7dedbf1ea73

--- source/.vuepress/.temp/pages/about/about.html.vue
+++
@@ -1,3 +0,0 @@
-<template><h1 id="about" tabindex="-1"><a class="header-anchor" href="#about" aria-hidden="true">#</a> About</h1>
-<p>.</p>
-</template>
--- source/projects/jpa.md
+++
@@ -1,4 +0,0 @@
-# greenzoo-jpa
-* JPA 예제 프로젝트
-
-* 참고사이트
